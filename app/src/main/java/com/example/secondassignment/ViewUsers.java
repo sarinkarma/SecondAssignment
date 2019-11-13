2f9fa3d56963d5b4c6746acf92342a5ea45485f7
Sarin Karmacharya <sarinkarma@gmail.com>
2019-11-13 23:11:48 +0545

    Initial commit

---
+++ app/src/main/java/com/example/secondassignment/ViewUsers.java
@@ -0,0 +1,63 @@
+package com.example.secondassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.secondassignment.Modal.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewUsers extends AppCompatActivity implements userRvAdapter.OnUserClickListener {
+
+    RecyclerView recyclerView;
+    List<User> users = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_users);
+
+        recyclerView = findViewById(R.id.rvUsers);
+
+        Intent intent = getIntent();
+        final List<User> userList = (List<User>) intent.getSerializableExtra("allusers");
+        users.addAll(userList);
+        String[] userNames = new String[userList.size()];
+
+        int userSize = userList.size();
+        for(int i = 0; i<userSize; i++ ){
+            Log.d("user", String.valueOf(userList.get(i)));
+        }
+
+        int i = 0;
+        for(User user:userList){
+            userNames[i] = user.getName();
+            i++;
+        }
+
+        userRvAdapter adapter = new userRvAdapter(userList, this, this);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onUserClickListener(int position) {
+        Intent intent = new Intent(this, UserDetail.class);
+        intent.putExtra("Name", users.get(position).getName());
+        intent.putExtra("Gender", users.get(position).getGender());
+        intent.putExtra("DoB", users.get(position).getDob());
+        intent.putExtra("Country", users.get(position).getCountry());
+        intent.putExtra("Phone", users.get(position).getPhone());
+        intent.putExtra("Email", users.get(position).getEmail());
+        intent.putExtra("image", users.get(position).getImg());
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/secondassignment/userRvAdapter.java
@@ -0,0 +1,85 @@
+package com.example.secondassignment;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.secondassignment.Modal.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class userRvAdapter extends RecyclerView.Adapter<userRvAdapter.userHolder> {
+
+    List<User> users = new ArrayList<>();
+    private Context context;
+    private OnUserClickListener onUserClickListener;
+
+    public userRvAdapter(List<User> users, Context context, OnUserClickListener onUserClickListener) {
+        this.users = users;
+        this.context = context;
+        this.onUserClickListener = onUserClickListener;
+    }
+
+    @NonNull
+    @Override
+    public userHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_layout, parent, false);
+        userHolder userHolder = new userHolder(view, onUserClickListener);
+        return userHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull userHolder holder, int position) {
+        final User user = users.get(position);
+        holder.tvName.setText(user.getName());
+        holder.imageView.setImageResource(user.getImg());
+
+        holder.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public class userHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        ImageView imageView;
+        TextView tvName;
+        OnUserClickListener onUserClickListener;
+
+        public userHolder(@NonNull View itemView, OnUserClickListener onUserClickListener) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.userImg);
+            tvName = itemView.findViewById(R.id.userName);
+
+            this.onUserClickListener = onUserClickListener;
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            onUserClickListener.onUserClickListener(getAdapterPosition());
+        }
+    }
+
+    public interface OnUserClickListener{
+        void onUserClickListener(int position);
+    }
+
+}
